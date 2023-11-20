bfbe2304a742472f4b2bb78aefe7e7e428055d41
Marvin Ogot <marvin.ogot@gmail.com>
2023-11-20 05:28:31 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  // eslint-disable-next-line no-unused-vars
+  const [categories, setCategories] = useState(CATEGORIES);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleCategoryFilterChange = (selectedCategory) => {
+    setSelectedCategory(selectedCategory);
+    const filteredTasks =
+      selectedCategory === "All"
+        ? TASKS
+        : TASKS.filter((task) => task.category === selectedCategory);
+    setTasks(filteredTasks);
+  };
+
+  const handleTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+  const handleTaskDelete = (taskToDelete) => {
+    const updatedTasks = tasks.filter((task) => task !== taskToDelete);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={categories}
+        onSelectCategory={handleCategoryFilterChange}
+        selectedCategory={selectedCategory}
+      />
+      <NewTaskForm
+        categories={categories}
+        onTaskFormSubmit={handleTaskFormSubmit}
+      />
+      <TaskList tasks={tasks} onDelete={handleTaskDelete}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onSelectCategory, selectedCategory }) {
   return (
     <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button
+          key={category}
+          onClick={() => onSelectCategory(category)}
+          className={selectedCategory === category ? "selected" : ""}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
